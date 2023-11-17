d1e3db1c5feae4733f4e676502023ed96b62d8e3
Willian Bogler <willian.silva@pti.org.br>
2023-11-17 17:03:34 -0300

    jwt

---
+++ Todojwt/src/main/java/com/pizzaria/TodojwtApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodojwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodojwtApplication.class, args);
+	}
+
+}
